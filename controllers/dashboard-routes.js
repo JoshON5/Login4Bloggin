7f2e0dbdb91ea4d3beda10aada0e05e893780668
Joshua Nichols <joshua@DOOMBOT-2.local>
2023-09-11 23:07:05 -0500

    fixed a naming error in dashboard columns of name to title

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -8,7 +8,7 @@ router.get('/',  withAuth, async (req, res) => {
             where: {
                 user_id: req.session.user_id
             },
-            attributes:['id', 'name', 'blogpost', 'date_created'],
+            attributes:['id', 'title', 'blogpost', 'date_created'],
             order: [['date_created', 'DESC']],
             include: [
                 {
